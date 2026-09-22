6658a7d5472b3f1c703cf0cde51d8ce8aa3c322d
kamlesh <kamlesh81473@gmail.com>
2026-09-22 22:23:38 +0530

    new code

---
+++ Array/update.java
@@ -0,0 +1,18 @@
+public class update {
+    public static void update_element(int marks[]){
+        for(int i=0; i<marks.length; i++){
+            marks[i] = marks[i] + 1;
+        }
+    }
+    public static void main(String[] args) {
+        int marks[] = {8,5,6,4,9};
+        update_element(marks);
+
+        for(int i=0; i<marks.length; i++){
+            System.out.print(marks[i]+ " ");
+        }
+        
+         System.out.println();
+    }
+    
+}
