954186d935955ab9a3e345fdb7c9944f89f0dd6e
kamlesh <kamlesh81473@gmail.com>
2026-09-22 23:03:58 +0530

    new code linearSearch

---
+++ Array/linearSearch.java
@@ -0,0 +1,25 @@
+ public class linearSearch{
+    public static int  linear_search(int numbers[], int key){
+        for(int i=0; i< numbers.length; i++){
+            if(numbers[i] == key){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+
+    public static void main(String args[] ){
+        int numbers[] = {2,4,6,8,10,12,14,16};
+        int key = 16;
+        int index = linear_search(numbers,key);
+        if(index == -1){
+            System.out.print("Not found");
+        }
+        else {
+             System.out.print("key is at index : " + index);
+        }
+        System.out.println();
+    }
+
+ }
