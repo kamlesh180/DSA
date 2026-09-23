c9f836a2c22ff25f7b20f4f2719b305c9baaac7a
kamlesh <kamlesh81473@gmail.com>
2026-09-24 02:03:04 +0530

    new code

--- Array/subArray.java
+++ Array/subArray.java
@@ -1,5 +1,6 @@
 public class subArray {
     public static void sub_array(int numbers[]){
+        int ts=0;
         //outer loop
         for(int i=0;i<numbers.length; i++){
             int start=i;
@@ -10,10 +11,12 @@ public class subArray {
                for(int k=start; k<=end ;k++){
                 System.out.print(numbers[k]+ " ");
                }
+               ts++;
                System.out.println();
             }
              System.out.println();
         }
+            System.out.println("Total subarray = " + ts);
 
     }
     public static void main(String args[] ){
